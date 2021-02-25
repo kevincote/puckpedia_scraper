246f8aea612aba0fe145d3886d0dcf7420119d7f
Kevin Côté <kevin.cote2@gmail.com>
2021-02-25 14:35:52 -0500

    update scrape url

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-url = "https://puckpedia.com/e/players?e=o8uvhq&sz=2000"
+url = "https://puckpedia.com/e/players?e=p3e67d&sz=2000#&c=[2,3,4,5,7,10,12,15,17,19,20,21,22,23,24]"
 
 w = requests.get(url)
 soup = BeautifulSoup(w.content)
@@ -19,4 +19,3 @@ for i, data_row in enumerate(data_rows):
 df.to_csv('output.csv')
 
 print('done')
-
